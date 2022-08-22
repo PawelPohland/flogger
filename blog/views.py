1dc12964704af8c3e0b9596ac81ef9d410dc1084
PawelPohland <pawel.pohland@gmail.com>
2022-08-22 13:11:47 +0200

    Add navigation bar and homepage template.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import session
+from flask import render_template
 
 
 blog_app = Blueprint('blog_app', __name__)
@@ -7,6 +8,4 @@ blog_app = Blueprint('blog_app', __name__)
 
 @blog_app.route('/')
 def index():
-    if session.get("full_name"):
-        return f"Hi, {session['full_name']}"
-    return 'Blog Homepage'
+    return render_template("blog/index.html")
